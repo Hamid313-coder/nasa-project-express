50ebd4c9264ac99ed55ee03fc0ad6f38a39fa29a
hamid313-coder <hamid.hussainy0@gmail.com>
2025-11-03 09:58:33 +0430

    first commit

---
+++ client/src/hooks/requests.js
@@ -0,0 +1,30 @@
+const baseUrl = "http://localhost:8000";
+
+async function httpGetPlanets() {
+  // Load planets and return as JSON.
+  const response = await fetch(`${baseUrl}/planets`);
+
+  return await response.json();
+}
+
+// Load launches, sort by flight number, and return as JSON.
+async function httpGetLaunches() {
+  const response = await fetch(`${baseUrl}/launches`);
+  const fetchedLaunches = await response.json();
+
+  return fetchedLaunches.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
+}
+
+async function httpSubmitLaunch(launch) {
+  // TODO: Once API is ready.
+  // Submit given launch data to launch system.
+}
+
+async function httpAbortLaunch(id) {
+  // TODO: Once API is ready.
+  // Delete launch with given ID.
+}
+
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
