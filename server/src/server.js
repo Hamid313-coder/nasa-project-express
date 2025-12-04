92706a32587fbc35b1bf0cdbfbbc8bd3b9298aac
hamid313-coder <hamid.hussainy0@gmail.com>
2025-12-04 09:26:15 +0430

    connecting to mongo db and making schemas are done

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,29 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 const { loadPlanetsData } = require("./models/planets.model");
-const { start } = require("repl");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+  "mongodb+srv://nasa-api:3glOE70dRTAfv02T@nasacluster.mbg2xcm.mongodb.net/nasa?appName=nasaCluster";
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", () => {
+  console.log("Mongo db connection error!");
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL, {
+    
+  });
+
   await loadPlanetsData();
 
   server.listen(PORT, () => {
